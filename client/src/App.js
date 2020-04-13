868c03c301ae3977defb42da10f4ff4bef44bf38
unknown <paulgray0813@gmail.com>
2020-04-13 14:04:41 -0700

    started client side integration and development of login screen

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import { Switch, Route, Redirect } from 'react-router-dom';
+import SignIn from "./components/SignIn"
+function App() {
+  return (
+<SignIn />
+   
+  )
+}
+
+
+export default App;
